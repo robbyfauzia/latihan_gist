c2d03a7cf609157b4a7303653755868c25ba17c6
robbyfauzia <robbyfauzia123@gmail.com>
2022-07-26 15:15:03 +0700

    gist

---
+++ soal1/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <fieldset>
+        <legend>Soal 1</legend>
+        <form action="" method="post">
+            <label for="ji">Jumlah Inputan</label>
+            <input type="number" name="number">
+            <input type="submit" value="Simpan" name="save">
+        </form>
+        
+    </fieldset>
+</body>
+</html>
